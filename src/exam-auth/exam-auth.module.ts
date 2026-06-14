7ab82c9a52e18bfc2255461e41c1a241ab9e6a64
LienThuan04 <lienthuan2004@gmail.com>
2026-06-14 09:56:37 +0700

    refactor(exam-auth): module tự đăng ký PassportModule + JwtStrategy

--- src/exam-auth/exam-auth.module.ts
+++ src/exam-auth/exam-auth.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ExamAuthController } from '@/exam-auth/exam-auth.controller';
 import { ExamAuthService } from '@/exam-auth/exam-auth.service';
+import { JwtStrategy } from '@/exam-auth/passport/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -18,6 +21,6 @@ import { ExamAuthService } from '@/exam-auth/exam-auth.service';
     }),
   ],
   controllers: [ExamAuthController],
-  providers: [ExamAuthService],
+  providers: [ExamAuthService, JwtStrategy],
 })
 export class ExamAuthModule {}
