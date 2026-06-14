274f37bb636232d4261abb7f290b06108f0fd8c1
LienThuan04 <lienthuan2004@gmail.com>
2026-06-14 09:56:57 +0700

    refactor(session): gỡ module session demo nền (JWT stateless)

--- src/session/dto/update-session.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateSessionDto } from '@/session/dto/create-session.dto';
-import { PartialType } from '@nestjs/swagger';
-
-export class UpdateSessionDto extends PartialType(CreateSessionDto) {}
--- src/session/session.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SessionService } from '@/session/session.service';
-import type { ISessionController } from '@/session/interfaces/session.controller.interface';
-
-@Controller('session')
-export class SessionController implements ISessionController {
-  constructor(private readonly sessionService: SessionService) {}
-
-}
